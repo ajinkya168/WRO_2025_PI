970a96685329d518f8e1fcacb5287ad6616213ca
ajinkya168 <ajinkyagiri28@gmail.com>
2025-08-30 16:01:42 +0530

    Obtacle challenge new

--- versionTest/Obstacle_Challenge_2025_Final.py
+++ versionTest/Obstacle_Challenge_2025_Final.py
@@ -1670,7 +1670,7 @@ def servoDrive(color_b, stop_evt, red_b, green_b, pink_b, counts, centr_y, centr
                                 if orange_flag:
                                     print(f"prev_distance: {prev_distance}, distance_right: {tf_r} diff: {abs(prev_distance - tf_r)}")
                                     p_flag = True
-                                    if tf_r <= 30 and (abs(prev_distance - tf_r) >= 10 and prev_distance > 0) and not pink_b.value:
+                                    if tf_r <= 35 and (abs(prev_distance - tf_r) >= 10 and prev_distance > 0) and not pink_b.value:
                                         p_pass = 2
                                         if p_pass == 2:
                                             p_past = False
@@ -1682,7 +1682,7 @@ def servoDrive(color_b, stop_evt, red_b, green_b, pink_b, counts, centr_y, centr
                                 elif blue_flag:
                                     
                                     print(f"prev_distance: {prev_distance}, distance_left: {tf_l}  diff: {abs(prev_distance - tf_l)}")
-                                    if tf_l <= 30 and (abs(prev_distance - tf_l) >= 10 and prev_distance > 0) and not pink_b.value:
+                                    if tf_l <= 35 and (abs(prev_distance - tf_l) >= 10 and prev_distance > 0) and not pink_b.value:
                                         p_pass = 2
                                         if p_pass == 2:
                                             p_past = False
