60d5884957c6313b28e5cca5773f0232a0133638
ajinkya168 <ajinkyagiri28@gmail.com>
2025-11-13 15:41:43 +0530

    abc

--- versionTest/Obstacle_Challenge_World_Final.py
+++ versionTest/Obstacle_Challenge_World_Final.py
@@ -1356,8 +1356,11 @@ def servoDrive(red_b, green_b, pink_b, counts, centr_y, centr_x, centr_y_red, ce
                                                 buff_time = time.time()                                            
                                     elif orange_flag:
                                         if counter % 4 == pink_wall_lane :
-                                            avoid_thres = 0.7
-                                            if ( tf_r <= 55 and tf_r > 0) or (time.time() - green_time > avoid_thres): 
+                                            if counter == 0:
+                                                avoid_thres = 1.5
+                                            else:
+                                                avoid_thres = 0.7
+                                            if ( tf_r <= 40 and tf_r > 0 and not green_b.value) or (time.time() - green_time > avoid_thres): 
                                                 g_flag = False
                                                 OBSTACLE_STATE = 3
                                                 buff_time = time.time()
@@ -1389,8 +1392,11 @@ def servoDrive(red_b, green_b, pink_b, counts, centr_y, centr_x, centr_y_red, ce
                                                 buff_time = time.time()                                            
                                     elif blue_flag:
                                         if counter % 4 == pink_wall_lane :
-                                            avoid_thresh = 0.7
-                                            if ( tf_l <= 55 and tf_l > 0) or (time.time() - red_time > avoid_thres): 
+                                            if counter == 0:
+                                                avoid_thres = 1.5
+                                            else:
+                                                avoid_thres = 0.7
+                                            if ( tf_l <= 40 and tf_l > 0 and not red_b.value) or (time.time() - red_time > avoid_thres): 
                                                 r_flag = False
                                                 OBSTACLE_STATE = 3
                                                 buff_time = time.time()
