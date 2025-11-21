036788e30caebdb2e714e7e9a2f9c9ba9ba2db32
ajinkya168 <ajinkyagiri28@gmail.com>
2025-11-21 19:14:35 +0530

    abc

--- versionTest/Obstacle_Challenge_World_Final.py
+++ versionTest/Obstacle_Challenge_World_Final.py
@@ -1342,7 +1342,8 @@ def servoDrive(red_b, green_b, pink_b, counts, centr_y, centr_x, centr_y_red, ce
                                     elif lidar_r.value > lidar_l.value and tfmini.distance_left < 100:
                                         print("Correcting wall to left")
                                         correctWall(45, tfmini.distance_left, heading_angle, head.value, orange_flag, blue_flag, pink_wall_lane, counter, True, False)
-                                    #correctPosition(setPointC, heading_angle, x, y, counter, blue_flag, orange_flag, head.value, centr_x_pink.value, centr_x_red.value, centr_x.value, centr_y.value, centr_y_red.value, centr_y_pink.value, tf_h, tf_l, tf_r, red_b.value, green_b.value, pink_wall_lane, abs(corr) )
+                                    else:
+                                        correctPosition(setPointC, heading_angle, x, y, counter, blue_flag, orange_flag, head.value, centr_x_pink.value, centr_x_red.value, centr_x.value, centr_y.value, centr_y_red.value, centr_y_pink.value, tf_h, tf_l, tf_r, red_b.value, green_b.value, pink_wall_lane, abs(corr) )
                                     p_flag = False
                                     g_flag = False
                                     r_flag = False
