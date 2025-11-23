b557c87c1c659179ec3e3fdbc7a9514a4ce7a019
ajinkya168 <ajinkyagiri28@gmail.com>
2025-11-23 19:44:01 +0530

    abc

--- versionTest/Obstacle_Challenge_Worlds.py
+++ versionTest/Obstacle_Challenge_Worlds.py
@@ -571,7 +571,7 @@ def servoDrive( red_b, green_b, pink_b, counts, centr_y, centr_x, centr_y_red, c
     setPointC = 0
     power = 95
     prev_power = 0
-    last_counter = 4#12
+    last_counter = 12
     counter = turn_t = current_time = gp_time = rp_time = buff = c_time = 0
     heading_angle = 0
     lap_finish_time = ( prev_distance ) = turn_trigger_distance = target_count = offset = button_STATE = exit_STATE = 0
