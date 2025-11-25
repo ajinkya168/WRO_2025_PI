41581a3ec96cdb42e2fd58e23a67ae0771dc1b86
ajinkya168 <ajinkyagiri28@gmail.com>
2025-11-25 10:59:49 +0000

    abc

--- versionTest/Obstacle_Challenge_Worlds.py
+++ versionTest/Obstacle_Challenge_Worlds.py
@@ -1325,7 +1325,7 @@ def servoDrive( red_b, green_b, pink_b, counts, centr_y, centr_x, centr_y_red, c
                                     x, y = enc.get_position( head.value, counts.value)
                                     tfmini.getTFminiData()
                                     correctReverseAngle( heading_angle, head.value, 2)
-                                    power = 75
+                                    power = 90
                                     prev_power = 0
                                     runMotor(power, 0)
                             
