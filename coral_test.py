903b0bd108a7fa823ae3e64a21bec660024d5b36
ajinkya168 <ajinkyagiri28@gmail.com>
2026-05-26 09:27:52 +0100

    abc

--- coral_test.py
+++ coral_test.py
@@ -73,7 +73,7 @@ try:
         now = time.time()
         fps = 1.0 / max(1e-3, (now - t_prev)); t_prev = now
         print(f"cls_name:{cls_name} fps: {fps} pair:{pairs} cx:{cx} cy:{cy}")
-        #cv2.imshow("Coral SSD Live", frame_bgr)
+        cv2.imshow("Coral SSD Live", frame_bgr)
         if cv2.waitKey(1) & 0xFF == 27:  # ESC
             break
 finally:
